ea5c143a9357271d39294252777c0b65a384975b
hayinanjay <72958200+hayinanjay@users.noreply.github.com>
2020-10-16 10:50:47 +0700

    Add files via upload

---
+++ src/pbo1/pkg10119005/latihan6/Mamalia.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10119005.latihan6;
+
+/**
+ *
+ * @author Hayin
+ * NAMA     : Hayin Ananta
+ * KELAS    : IF-1
+ * NIM      : 10119005
+ * Deskripsi Program : Kambing Static dan konstanta
+ */
+public class Mamalia {
+    //Variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+    
+}
+
+/**
+ *
+ * @author Hayin
+ */
+
